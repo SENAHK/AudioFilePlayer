ed0988b07627cdbae1e87d5fa94f1eef6c23a742
serenasdk <serena.sdk@eduge.ch>
2017-06-18 11:23:55 +0200

    commentaires

--- friends.js
+++ friends.js
@@ -1,11 +1,11 @@
 /* 
  
  * Auteur	: Michael Ramusi
- * Date	: 16 juin 2017 
+ * Date         : juin 2017 
  * Projet	: AudioFilePlayer
  * Copyright	: TPI 2017 - Michael RAMUSI
  * Fichier	: friends
- * Fonction	:
+ * Fonction	: Gère la récupération et l'affichage des amis
  
  */
 
